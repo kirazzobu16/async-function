37e1ba8226afbee142aea86ae393c1e512c042c0
Burak Kiraz <kirazzobu16@gmail.com>
2022-10-21 17:27:27 +0300

    proje tamamlandı.

---
+++ app.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+const getUsers =() => {
+    return new Promise(async(resolve,reject)=>{
+        const{data:users}=await axios ("https://jsonplaceholder.typicode.com/users/1")
+        
+        resolve(users)
+    })
+}
+
+const getPOSTS=() => {
+    return new Promise(async(resolve,reject)=>{
+        const{data}=await axios("https://jsonplaceholder.typicode.com/posts/1?userId=1")
+        resolve(data)
+    
+    })
+}
+
+ const getDATA =async()=>{
+    try {
+        const users= await getUsers()
+        const data =await getPOSTS()
+        console.log(users)
+        console.log(data)
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
+export default getDATA
---
+++ index.js
@@ -0,0 +1,3 @@
+import getDATA from "./app.js"
+
+console.log(getDATA())
